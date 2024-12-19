f898069508a734a3b51ca2461469b1b84e725fd0
linzhemin <linzhemin@qweib.com>
2024-12-19 15:43:41 +0800

    hot login

--- main.go
+++ main.go
@@ -6,40 +6,110 @@ import (
 	"github.com/eatmoreapple/openwechat"
 	openai "github.com/sashabaranov/go-openai"
 	"strings"
-	"time"
 )
 
 // TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 func main() {
 	bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式
+	reloadStorage := openwechat.NewFileHotReloadStorage("storage.json")
+	defer reloadStorage.Close()
 	messages := make([]openai.ChatCompletionMessage, 0)
-	config := openai.DefaultConfig("your chatGPT api key here")
+	messages = append(messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleSystem,
+		Content: "你的名字是小纯洁，你现在在一个工作群里中回答大家的提问，如果是冒犯你的话语直接毒舌回复，会玩v50(KFC疯狂星期四)的梗",
+	})
+	messages = append(messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleSystem,
+		Content: "技术类的问题正常回答",
+	})
+	messages = append(messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleSystem,
+		Content: "你的主人是敏哥",
+	})
+
+	config := openai.DefaultConfig("sk-QcL0Hqjfpjh6D9stoYGRMRkzMAL0rpjgiuAskiFKK2Mi8wKf")
 	config.BaseURL = "https://api.chatanywhere.tech"
 	client := openai.NewClientWithConfig(config)
+	// 注册登陆二维码回调
+	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
+	// 登陆
+	if err := bot.HotLogin(reloadStorage, openwechat.NewRetryLoginOption()); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 获取登陆的用户
+	self, err := bot.GetCurrentUser()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 获取所有的好友
+	friends, err := self.Friends()
+	fmt.Println(friends, err)
+
+	// 获取所有的群组
+	groups, err := self.Groups()
+	fmt.Println(groups, err)
+	memberMap := make(map[string]string)
+	for _, group := range groups {
+		fmt.Println(group.NickName)
+		members, err := group.Members()
+		if err == nil {
+			// convert members to map<UserName, User>
+			for _, member := range members {
+				memberMap[member.UserName] = member.NickName
+				fmt.Println(member.NickName)
+			}
+		}
+	}
+
+	// print member cache size
+	fmt.Println("member cache size:", len(memberMap))
+
 	// 注册消息处理函数
 	bot.MessageHandler = func(msg *openwechat.Message) {
 		if msg.IsText() {
 			// 打印 msg.Content
 			fmt.Println(msg.Content)
 		}
-		if msg.IsAt() {
-			// check msg.Content starts with "@akka "
-			if !strings.Contains(msg.Content, "@akka") {
-				return
-			}
-			now := time.Now().Hour()
-			if now <= 7 || now >= 20 {
-				msg.ReplyText(openwechat.Emoji.Shhh + "~ 晚安" + openwechat.Emoji.Sleep + openwechat.Emoji.Sleep)
-				return
-			}
-			// 截取 "@akka "后面的部分
-			question := strings.ReplaceAll(msg.Content, "@akka", "")
-			fmt.Printf(question)
 
+		if msg.IsAt() || msg.IsTickledMe() {
+			// check msg.Content starts with "@akka "
+			if msg.IsAt() && !strings.Contains(msg.Content, "@akka") {
+				return
+			}
+			sender, err := msg.SenderInGroup()
+			fromUserName := ""
+			if sender != nil {
+				fromUserName = memberMap[sender.UserName]
+			}
+			question := ""
+			// 截取 "@akka "后面的部分
+			if msg.IsTickledMe() {
+				question = "拍了拍你"
+			} else {
+				question = strings.ReplaceAll(msg.Content, "@akka", "")
+			}
+			fmt.Printf(question)
 			if question == "skadoosh" {
 				// 清空messages
 				messages = make([]openai.ChatCompletionMessage, 0)
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "你的名字是小纯洁，你现在在一个工作群里中回答大家的提问，如果是冒犯你的话语直接毒舌回复，会玩v50(KFC疯狂星期四)的梗",
+				})
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "技术类的问题正常回答",
+				})
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "你的主人是敏哥",
+				})
+
 			}
 
 			// 加入上下文
@@ -47,6 +117,8 @@ func main() {
 				Role:    openai.ChatMessageRoleUser,
 				Content: question,
 			})
+			// messages size
+			fmt.Println("context size:", len(messages))
 			answer := ""
 			resp, err := client.CreateChatCompletion(
 				context.Background(),
@@ -58,6 +130,18 @@ func main() {
 			if err != nil {
 				fmt.Printf("ChatCompletion error: %v\n", err)
 				messages = make([]openai.ChatCompletionMessage, 0)
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "你的名字是小纯洁，你现在在一个工作群里中回答大家的提问，如果是冒犯你的话语直接毒舌回复，会玩v50(KFC疯狂星期四)的梗",
+				})
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "技术类的问题正常回答",
+				})
+				messages = append(messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "你的主人是敏哥",
+				})
 				messages = append(messages, openai.ChatCompletionMessage{
 					Role:    openai.ChatMessageRoleUser,
 					Content: question,
@@ -84,37 +168,16 @@ func main() {
 				Role:    openai.ChatMessageRoleAssistant,
 				Content: answer,
 			})
+
+			// replace "小纯洁：" to empty
+			answer = strings.ReplaceAll(answer, "小纯洁：", "")
+			// if not empty fromUserName
+			if fromUserName != "" {
+				answer = "@" + fromUserName + " " + answer
+			}
 			msg.ReplyText(answer)
-
-		}
-		if msg.IsTickledMe() {
-			msg.ReplyText("再拍报警了")
 		}
 	}
-	// 注册登陆二维码回调
-	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
-
-	// 登陆
-	if err := bot.Login(); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// 获取登陆的用户
-	self, err := bot.GetCurrentUser()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// 获取所有的好友
-	friends, err := self.Friends()
-	fmt.Println(friends, err)
-
-	// 获取所有的群组
-	groups, err := self.Groups()
-	fmt.Println(groups, err)
-
 	// 阻塞主goroutine, 直到发生异常或者用户主动退出
 	bot.Block()
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/sashabaranov/go-openai"
 	"strings"
 	"testing"
 	"time"
 )
 
 func TestChatGPT(t *testing.T) {
-	/*config := openai.DefaultConfig("sk-QcL0Hqjfpjh6D9stoYGRMRkzMAL0rpjgiuAskiFKK2Mi8wKf")
+	config := openai.DefaultConfig("sk-QcL0Hqjfpjh6D9stoYGRMRkzMAL0rpjgiuAskiFKK2Mi8wKf")
 	config.BaseURL = "https://api.chatanywhere.tech"
 	client := openai.NewClientWithConfig(config)
 	resp, err := client.CreateChatCompletion(
@@ -17,15 +19,37 @@ func TestChatGPT(t *testing.T) {
 			Model: openai.GPT3Dot5Turbo,
 			Messages: []openai.ChatCompletionMessage{
 				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "你的名字是akka，你现在在一个工作群里中回答大家的提问，如果是冒犯你的话语直接毒舌回复，你认识锤妹(女)、剑平、芹菜(女)、龙哥、伟哥、砖哥、斌哥、鑫和冲哥，会玩v50(KFC疯狂星期四)的梗",
+				},
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "允许使用别人指定的名字更换名字",
+				},
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "技术类的问题正常回答",
+				},
+				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: "hi",
+					Content: "你叫锤妹吧",
 				},
 			},
 		},
 	)
 	if err == nil {
 		fmt.Println(resp.Choices[0].Message.Content)
-	}*/
+	}
+
+}
+
+func TestUtils(t *testing.T) {
+	// 获取当前时间 24小时制 小时
+	now := time.Now().Hour()
+	fmt.Println(now)
+
+	//fmt.Println(GetRandomString(10))
+
 	msg := "@akka 鲁迅为什么暴打周树人"
 	if !strings.HasPrefix(msg, "@akka ") {
 		return
@@ -42,12 +66,4 @@ func TestChatGPT(t *testing.T) {
 	msg = strings.ReplaceAll(msg, "@akka", "")
 
 	fmt.Printf(msg)
-}
-
-func TestUtils(t *testing.T) {
-	// 获取当前时间 24小时制 小时
-	now := time.Now().Hour()
-	fmt.Println(now)
-
-	//fmt.Println(GetRandomString(10))
 }
